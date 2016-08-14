51ae41a8cc913321a2b8ad7b2fe8dba275f1736c
Daniel Frank <daniel.frank@lin-digital.com>
2016-08-13 22:01:24 -0700

    Add blocking operation to see why bad request handling method is bad

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,11 +1,18 @@
 function start() {
   console.log("Request handler 'start' was called.");
-  return "Hello start"
+
+  function sleep(milliSeconds) {
+    var startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + milliSeconds);
+  }
+
+  sleep(10000);
+  return "Hello start";
 }
 
 function upload() {
   console.log("Request handler 'upload' was called.");
-  return "Hello upload"
+  return "Hello upload";
 }
 
 exports.start = start;
